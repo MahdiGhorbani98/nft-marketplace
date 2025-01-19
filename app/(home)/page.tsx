33a34c8400edad28358160ad6aad95bf65b04a21
oliver <mahdig502@gmail.com>
2025-01-19 17:49:32 +0330

    feat: BestCreators section

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
-import Banner from "@/components/Banner";
 import React from "react";
+import Banner from "@/components/features/home/Banner";
+import BestCreators from "@/components/features/home/BestCreators";
 
 export default function Home() {
   console.log("page.tsx");
@@ -15,6 +16,7 @@ export default function Home() {
           childStyles="text-xl sm:text-2xl md:text-4xl   text-left"
           parentStyle="justify-start mb-7  md:h-72 h-44 sm:h-60 sm:p-12 p-4  rounded-3xl"
         />
+        <BestCreators />
       </div>
     </div>
   );
